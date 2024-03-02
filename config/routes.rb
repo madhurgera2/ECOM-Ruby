22e060301b2d9e64684dbd39f3f4eda66b93e7e4
madhurgera2 <madhurgera2@gmail.com>
2024-03-02 22:55:49 +0530

    Add admin functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root: "home#index"
+  root "home#index";
+  authenticate :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
+
+  get "admin" => "admin#index"
 end
